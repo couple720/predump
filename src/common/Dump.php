a7de0321095031ae31695d3dacb975b687de8f5f
lei <18264315@qq.com>
2023-12-28 09:02:49 +0800

    格式化打印功能完善

--- src/common/Dump.php
+++ src/common/Dump.php
@@ -4,12 +4,18 @@ namespace Dswl\Predump\common;
 
 class Dump
 {
+    /**
+     * 格式化打印
+     */
     public static function p($obj = null)
     {
         echo "<pre>";
         var_dump($obj);
         echo "</pre>";
     }
+    /**
+     * 格式化打印并且停止
+     */
     public static function pd($obj = null)
     {
         self::p($obj);
